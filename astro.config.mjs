0b630460bb066f3d63ae65819e969ffbda283417
Kacper Chojnacki <kacper.chojnacki23@gmail.com>
2026-05-21 15:20:53 +0200

    style: configure project for free github pages hosting at /portfolio/

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kacperchojnacki.pl',
+  site: 'https://kacper-chojnacki.github.io',
+  base: '/portfolio',
   output: 'static',
   vite: {
     plugins: [tailwindcss()],
